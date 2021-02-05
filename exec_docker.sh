cb11ba1aef1d1f40c5590eaf38da0442c359ad3b
keebmyn <66120720+keebmyn@users.noreply.github.com>
2021-02-05 13:31:16 +0900

    Update exec_docker.sh

--- exec_docker.sh
+++ exec_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run -it --name nia -v /home/smart30/Yonsei_Dataset/train:/Last_Land/data --gpus all nia-landmark
+sudo docker run -it --name nia -v /home/smart30/Yonsei_Dataset/train/:/Landmark/data --gpus all  nia-landmark
